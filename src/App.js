497bee81e2016fa7ccd112523d2726f5d1b5304c
shiv5ank <Shivank0596@gmail.com>
2023-07-05 22:27:29 +0530

    unsplash

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const [fetchData, setFetchData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("Sports cars");
+
+  useEffect(() => {
+    fetch(
+      `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=qd11Z665L1ygx5qj7YBwhDZexk3PLM7xO_83uHue95w`
+    )
+      .then((res) => res.json())
+      .then((data) => setFetchData(data.results));
+  }, [query]);
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+  function getSearch(e) {
+    e.preventDefault();
+    setQuery(search);
+
+    setSearch("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>UNSPLASH</h2>
+      <form className="form" onSubmit={getSearch}>
+        <input
+          className="search_container"
+          type="search"
+          placeholder="Search Image Name..."
+          aria-label="Search"
+          value={search}
+          onChange={updateSearch}
+        />
+        <button id="btn" className="" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="container">
+        {fetchData.map((item, key) => {
+          return (
+            <div className="images" key={key}>
+              <img
+                src={item.urls.small}
+                style={{ width: "350px", height: "auto" }}
+                alt="images"
+              />
+              <a href={item.links.download} target="_blank" rel="noreferrer">
+                download
+              </a>
+              <h6>user :{item.user.name}</h6>
+              <h6>created_at :{item.created_at}</h6>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
-
-export default App;
